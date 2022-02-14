dbde94629ac682e65ae55442dc7ef7fcfc6f8379
Pia <piatorain@yahoo.com>
2022-02-14 12:13:52 -0800

    Started on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import PageHeader from '../components/PageHeader';
+import { Intro } from '../components/HomeIntro';
 
 import SectionBreak from '../components/SectionBreak';
 import { Row, Col } from 'reactstrap';
 
 const Home = (props) => {
     return (
-        <PageHeader pageTitle={"Home"} />
+        <div className="home-bg ">
+            {/* <PageHeader pageTitle={"Home"} /> */}
+            <div className="container py-5">
+                <Intro />
+            </div>
+        </div>
     );
 }
 
