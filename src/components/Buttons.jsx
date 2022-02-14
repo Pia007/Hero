ecfa73ab352203749a70bdbc536114c49134eb46
Pia <piatorain@yahoo.com>
2022-02-14 12:17:01 -0800

    Corrected syntax

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 
-function Buttons({ color,clickHandler, btnText, className }) {
+function Buttons({ color, clickHandler, btnText, className }) {
     return (
         <div>
             <Button 
