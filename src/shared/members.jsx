8a1fd78b0830519c18aced745058db8b04fd6fdd
Pia <piatorain@yahoo.com>
2022-02-14 17:55:11 -0800

    Created Memeber Data

---
+++ src/shared/members.jsx
@@ -0,0 +1,42 @@
+export const MEMBERS = [
+    {
+        name: "Lorem ipsum",
+        title: "Executive Director"
+    },
+    {
+        name: "Lorem ipsum",
+        title: "Administrative Operations Manager"
+    },
+    {
+        name: "Lorem ipsum",
+        title: "Logistics & Transportation"
+    },
+    {
+        name: "Lorem ipsum",
+        title: "Food Drive Administrator"
+    },
+    {
+        name: "Lorem ipsum",
+        title: "Fundraising"
+    },
+    {
+        name: "Lorem ipsum",
+        title: "Marketing & Public Relations"
+    },
+    {
+        name: "Lorem ipsum",
+        title: "Mission Partnerships"
+    },
+    {
+        name: "Lorem ipsum",
+        title: "Volunteer Management"
+    },
+    {
+        name: "Lorem ipsum",
+        title: "Volunteer Coordinator"
+    },
+    {
+        name: "Lorem ipsum",
+        title: "Progams & Events"
+    }
+];
