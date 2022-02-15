5f6fa4d15835a597cb10a8859ff5f5c58c8b2959
Pia <piatorain@yahoo.com>
2022-02-14 18:15:30 -0800

    Updated Accordion

--- src/components/MissionAccordion.jsx
+++ src/components/MissionAccordion.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from 'react';;
 
 export const MissionAccordion = ({ name, description  }) => {
     const [isActive, setIsActive] = useState(false);
@@ -6,10 +6,10 @@ export const MissionAccordion = ({ name, description  }) => {
     return (
         <div className="mission-text accordion-item px-3 px-sm-0">
             <div className="bg-primary accordion-title" onClick={() => setIsActive(!isActive)}>
-                <h4 className="text-light mb-0">{name}</h4>
+                <h4 className="text-light mb-0"><strong>{name}</strong></h4>
                 <div className="">{isActive ? '-' : '+'}</div>
             </div>
-            {isActive && <div className="text-primary p-1 accordion-description mb-1 px-3">{description}</div>}
+            {isActive && <div className="text-primary p-1 accordion-description mb-1 px-3"><strong>{name}</strong>, {description}</div>}
         </div>
 
     );
