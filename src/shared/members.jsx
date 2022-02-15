2f2471f70081fb9e250819a79d18a1839ae890b7
Pia <piatorain@yahoo.com>
2022-02-15 09:57:57 -0800

    Added unique keys to data

--- src/shared/members.jsx
+++ src/shared/members.jsx
@@ -1,41 +1,50 @@
 export const MEMBERS = [
     {
+        id: 0,
         name: "Lorem ipsum",
         title: "Executive Director"
     },
     {
+        id: 1,
         name: "Lorem ipsum",
         title: "Administrative Operations Manager"
     },
     {
+        id: 2,
         name: "Lorem ipsum",
         title: "Logistics & Transportation"
     },
     {
+        id: 3,
         name: "Lorem ipsum",
         title: "Food Drive Administrator"
     },
     {
+        id: 4,
         name: "Lorem ipsum",
         title: "Fundraising"
     },
     {
+        id: 5,
         name: "Lorem ipsum",
         title: "Marketing & Public Relations"
     },
     {
+        id: 6,
         name: "Lorem ipsum",
         title: "Mission Partnerships"
     },
     {
+        id: 7,
         name: "Lorem ipsum",
         title: "Volunteer Management"
     },
-    {
+    {   id: 8,
         name: "Lorem ipsum",
         title: "Volunteer Coordinator"
     },
     {
+        id: 9,
         name: "Lorem ipsum",
         title: "Progams & Events"
     }
--- src/shared/sponsors.jsx
+++ src/shared/sponsors.jsx
@@ -1,17 +1,21 @@
 export const SPONSORS = [
     {
+        id: 0,
         name: "Alpha Food Services, Inc.",
         description: "is a family business that has proudly served northern New Jersey for over 75 years. Over the years the business has grown to include 8 restaurants, 4 food trucks, 2 food distribution warehouses and a food pantry. When the pandemic hit, Alpha Food Services was able toassist in mediating the food crisis. They have partnered with Host A Healthcare Hero to provide health meals to healthcare workers."
     },
     {
+        id: 1,
         name:"Choprah Health & Wellness, LLC.",
         description: "  is a non-profit health and wellness center that provides free mental health and holistic services to the community. They have been in service for 30 years and have partnered with Host A Healthcare Hero to provide free mental health and holistic services to healthcare workers."
     },
     {
+        id: 2,
         name:"Cutz 4 All, LLC.",
         description: "has proudly served northern New Jersey for over 25 years. Over the years, with 5 locations, they have worked with several foundations to provide grooming services for cancer patients, disabled Americans and victims of domestic violence. They are dedicated to working with Host A Healthcare Hero to extend free services to healthcare workers."
     },
     {
+        id: 3,
         name:"Jersey Strength & Fitness, LLC.",
         description: " originated on the boardwalk of Atlantic City. After retiring with 7 locations in John Fitness wanted to give back and began working with several organizations to provide functional training to individuals with Parkinson's disease. That was 15 years ago. Since then, Jersey S&F has open 4 new locations throughout northern New Jersey soley dedicated to the community. Jersey Strength & Fitness has partnered with Host A Healthcare Worker to extend free fitness services to healthcare workers."
     }
