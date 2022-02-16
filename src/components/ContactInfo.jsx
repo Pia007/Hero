6adb651163104ce5436b2eb6511addf2e2ff30ad
Pia <piatorain@yahoo.com>
2022-02-15 18:39:47 -0800

    text to primary

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -8,7 +8,7 @@ const ContactInfo = (props) =>{
             {/* Contact Info */}
             <Fade bottom cascade>
                 <Row className="row-content pb-0 mb-4">
-                    <Col md={7} className="text-success">
+                    <Col md={7} className="text-primary">
                         <p className="mission-text"><strong>Host A Healthcare Hero</strong> relies heavily on the strength of community partners and
                             volunteers to provide healthcare heroes with appreciative care.
                         </p>
@@ -24,23 +24,23 @@ const ContactInfo = (props) =>{
                     </Col>
                 </Row>
 
-                <Row className=" p-0 text-success">
-                    <Col sm={6} className="text-success">
-                        <h5 className="mb-0 mission-text text-success"><u className="">Address:</u></h5>
+                <Row className=" p-0 text-primary">
+                    <Col sm={6} className="text-primary">
+                        <h5 className="mb-0 mission-text "><u className="">Address:</u></h5>
                         <address className="address-info">
                             2124 W Heroes LN, Suite 99<br />
                             Fictitious City, NJ 01007<br />
                         </address>
-                        <h5 className="mb-0 mission-text text-success"><u className="">Hours:</u></h5>
+                        <h5 className="mb-0 mission-text "><u className="">Hours:</u></h5>
                         <address className=" address-info">
                             Monday - Friday<br />
                             9AM - 3PM<br />
                             Or call us.
                         </address>
 
-                        <h5 className="mb-0 text-success mission-text"><u className="">Phone & Email</u></h5>
-                        <a role="button" className="btn btn-link p-0 text-success address-info" href="tel:+19737774321"><i className="fa fa-phone" ></i> 1-973-777-4321</a><br />
-                        <a role="button" className="btn btn-link p-0 text-success address-info" href="mailto:notARealEmail@NotReal.org"><i className="fa fa-envelope-o" ></i> info@healthcareheroes.org</a>
+                        <h5 className="mb-0 text-primary mission-text"><u className="">Phone & Email</u></h5>
+                        <a role="button" className="btn btn-link p-0 text-primary address-info" href="tel:+19737774321"><i className="fa fa-phone" ></i> 1-973-777-4321</a><br />
+                        <a role="button" className="btn btn-link p-0 text-primary address-info" href="mailto:notARealEmail@NotReal.org"><i className="fa fa-envelope-o" ></i> info@healthcareheroes.org</a>
                     </Col>
                 </Row>
             </Fade>
