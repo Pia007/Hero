17cfc46be8d30242f9be858ffe14d7429c404148
Pia <piatorain@yahoo.com>
2022-02-16 19:57:55 -0800

    Update fetch actions

---
+++ src/redux/ActionCreators.jsx
@@ -0,0 +1,48 @@
+// Create ActionCreators.jsx:
+import * as ActionTypes from './ActionTypes';
+import { SPONSORS } from '../shared/sponsors';
+import { MEMBERS } from '../shared/members';
+
+// Fetches sponsors from the store
+export const fetchSponsors = () => dispatch => {
+    dispatch(sponsorsLoading());
+    dispatch(addSponsors(SPONSORS));
+};
+
+export const sponsorsLoading = () => ({
+    type: ActionTypes.SPONSORS_LOADING
+});
+
+export const sponsorsFailed = errMess => ({
+    type: ActionTypes.SPONSORS_FAILED,
+    payload: errMess
+});
+
+export const addSponsors = sponsors => ({
+    type: ActionTypes.ADD_SPONSORS,
+    payload: sponsors
+
+});
+
+
+export const fetchMembers = () => dispatch => {
+    dispatch(membersLoading());
+    dispatch(addMembers(MEMBERS));
+}
+
+export const membersLoading = () => ({
+    type: ActionTypes.MEMBERS_LOADING
+});
+
+export const membersFailed = errMess => ({
+    type: ActionTypes.MEMBERS_FAILED,
+    payload: errMess
+});
+
+export const addMembers = members => ({
+    type: ActionTypes.ADD_MEMBERS,
+    payload: members
+
+});
+
+
