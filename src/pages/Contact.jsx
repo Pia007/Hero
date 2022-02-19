33d1e4f6459222f7b61ee2b63c191a46b46f775b
Pia <piatorain@yahoo.com>
2022-02-18 20:57:02 -0800

    Funtional Component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,9 +2,8 @@
 import PageHeader from '../components/PageHeader';
 import ContactInfo from '../components/ContactInfo';
 import ContactForm from '../components/ContactForm';
-import Fade from 'react-reveal/Fade';
 
-function Contact() {
+const Contact = () => {
         return (
             <div className="home-bg">
                 <PageHeader siteLocation={"Contact"} pageTitle={"Contact Us"} />
