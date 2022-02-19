28e80e8ba599d0618a3373591b37b8fd8531a397
Pia <piatorain@yahoo.com>
2022-02-18 18:37:57 -0800

    consolidata faq data

---
+++ src/shared/faqsData.jsx
@@ -0,0 +1,238 @@
+export const FAQS = [
+    {
+        id: 0,
+        category: "Donate",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 1,
+        category: "Donate",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 2,
+        category: "Donate",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 3,
+        category: "Donate",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 4,
+        category: "Donate",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 5,
+        category: "Donate",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 6,
+        category: "Donate",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 7,
+        category: "Donate",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {   id: 8,
+
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 9,
+        category: "Donate",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 10,
+        category: "Sponsor",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 11,
+        category: "Sponsor",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 12,
+        category: "Sponsor",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 13,
+        category: "Sponsor",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 14,
+        category: "Sponsor",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 15,
+        category: "Sponsor",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 16,
+        category: "Sponsor",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 17,
+        category: "Sponsor",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {   id: 18,
+        category: "Sponsor",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 19,
+        category: "Sponsor",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 20,
+        category: "Volunteer",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 21,
+        category: "Volunteer",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 22,
+        category: "Volunteer",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 23,
+        category: "Volunteer",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 24,
+        category: "Volunteer",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 25,
+        category: "Volunteer",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 26,
+        category: "Volunteer",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 27,
+        category: "Volunteer",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {   id: 28,
+        category: "Volunteer",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 29,
+        category: "Volunteer",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 30,
+        category: "General",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 31,
+        category: "General",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 32,
+        category: "General",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 33,
+        category: "General",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 34,
+        category: "General",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 35,
+        category: "General",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 36,
+        category: "General",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 37,
+        category: "General",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {   id: 38,
+        category: "General",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    },
+    {
+        id: 39,
+        category: "General",
+        question: "Lorem ipsum",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi ipsam tempora aliquam. Facilis qui impedit velit fugiat veritatis quia."
+    }
+];
