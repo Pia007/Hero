f6779d95923dbd3ff467933305ac2917835fa64e
Pia <piatorain@yahoo.com>
2022-02-18 20:57:46 -0800

    Added rendering to page and styled

--- src/pages/Faqs.jsx
+++ src/pages/Faqs.jsx
@@ -1,15 +1,118 @@
 import PageHeader from '../components/PageHeader';
-// import ContactInfo from '../components/ContactInfo';
-// import ContactForm from '../components/ContactForm';
+import AccordionItem from '../components/AccordionItem';
+import SectionBreak from '../components/SectionBreak';
+import Fade from 'react-reveal/Fade';
+import { Row, Col } from 'reactstrap';
 
-const Faqs = () => {
+const RenderFaq = ({ faq }) => {
+    return (
+        <AccordionItem name={faq.question} description={faq.answer} className="faq-accordion-description"/>
+    );
+}
+
+const Faqs = (props) => {
+
+    const generalFaqs = props.faqs?.filter(faq => faq.category === "General").map(filteredFaq => { 
         return (
-            <div className="home-bg">
-                <PageHeader siteLocation={"FAQs"} pageTitle={"Frequently Asked Questions"} />
-                {/* <ContactInfo />
-                <ContactForm /> */}
+            <div key={filteredFaq.id} className="col p-0">
+                <RenderFaq
+                    faq = {filteredFaq}
+                />
             </div>
         );
+    });
+
+    const volunteerFaqs = props.faqs?.filter(faq => faq.category === "Volunteer").map(filteredFaq => { 
+        return (
+            <div key={filteredFaq.id} className="col p-0">
+                <RenderFaq
+                    faq = {filteredFaq}
+                />
+            </div>
+        );
+    });
+
+    const donateFaqs = props.faqs?.filter(faq => faq.category === "Donate").map(filteredFaq => { 
+        return (
+            <div key={filteredFaq.id} className="col p-0">
+                <RenderFaq
+                    faq = {filteredFaq}
+                />
+            </div>
+        );
+    });
+
+    const sponsorFaqs = props.faqs?.filter(faq => faq.category === "Sponsor").map(filteredFaq => { 
+        return (
+            <div key={filteredFaq.id} className="col p-0">
+                <RenderFaq
+                    faq = {filteredFaq}
+                />
+            </div>
+        );
+    });
+    
+    return (
+        <div className="home-bg">
+            <div className="container">
+                <PageHeader siteLocation={"FAQs"} pageTitle={"Frequently Asked Questions"} />
+                <Fade bottom cascade>
+                    <Row className="row-content " id="genFacts">
+                        <Col xs={12} className="px-sm-0">
+                            <Fade bottom cascade>
+                            <h2 className="text-primary my-3">General </h2>
+                            </Fade>
+                        </Col>
+                        <Col xs={12} className="p-0">
+                            {generalFaqs}
+                        </Col>
+                    </Row>
+                </Fade>
+                <SectionBreak />
+
+                <Fade bottom cascade>
+                    <Row className="row-content " id="genFacts">
+                        <Col xs={12} className="px-sm-0">
+                            <Fade bottom cascade>
+                            <h2 className="text-primary my-3">Volunteer</h2>
+                            </Fade>
+                        </Col>
+                        <Col xs={12} className="p-0">
+                            {volunteerFaqs}
+                        </Col>
+                    </Row>
+                </Fade>
+                <SectionBreak />
+
+                <Fade bottom cascade>
+                    <Row className="row-content " id="genFacts">
+                        <Col xs={12} className="px-sm-0">
+                            <Fade bottom cascade>
+                            <h2 className="text-primary my-3">Sponsors</h2>
+                            </Fade>
+                        </Col>
+                        <Col xs={12} className="p-0">
+                            {sponsorFaqs}
+                        </Col>
+                    </Row>
+                </Fade>
+                <SectionBreak />
+
+                <Fade bottom cascade>
+                    <Row className="row-content " id="genFacts">
+                        <Col xs={12} className="px-sm-0">
+                            <Fade bottom cascade>
+                            <h2 className="text-primary my-3">Donate</h2>
+                            </Fade>
+                        </Col>
+                        <Col xs={12} className="p-0">
+                            {donateFaqs}
+                        </Col>
+                    </Row>
+                </Fade>
+            </div>
+        </div>
+    );
     
 }
 
