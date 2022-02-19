2ac81f5697026d0dbe8d3c64040a68be9b9fc5a8
Pia <piatorain@yahoo.com>
2022-02-18 18:08:29 -0800

    Created FAQ page

---
+++ src/pages/Faqs.jsx
@@ -0,0 +1,16 @@
+import PageHeader from '../components/PageHeader';
+// import ContactInfo from '../components/ContactInfo';
+// import ContactForm from '../components/ContactForm';
+
+const Faqs = () => {
+        return (
+            <div className="home-bg">
+                <PageHeader siteLocation={"FAQs"} pageTitle={"Frequently Asked Questions"} />
+                {/* <ContactInfo />
+                <ContactForm /> */}
+            </div>
+        );
+    
+}
+
+export default Faqs;
