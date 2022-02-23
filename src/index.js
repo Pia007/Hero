820df16a6e289b4bc65bc24bb725718b3b3df624
Pia <piatorain@yahoo.com>
2022-02-22 16:49:09 -0800

    Fonts

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import 'font-awesome/css/font-awesome.css';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 import 'bootswatch/dist/spacelab/bootstrap.min.css';
+import 'typeface-offside';
+import 'typeface-heebo';
+import 'typeface-montserrat';
+import 'typeface-open-sans';
+import 'typeface-roboto';
+import 'typeface-ubuntu';
 import App from './App';
 
 ReactDOM.render(
