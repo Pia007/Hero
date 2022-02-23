7dce437ba3d0fe4f10b04a9343d36d717eac9db7
Pia <piatorain@yahoo.com>
2022-02-22 16:48:04 -0800

    Added FaqCrumbs Comp

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -3,7 +3,7 @@ import { Breadcrumb, BreadcrumbItem, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 
-export const SiteCrumbs = ({ siteLocation }) => {
+export const SiteCrumbs = ({ siteLocation, faqLocationOne }) => {
     return (
         <Breadcrumb className="breadcrumb mb-0">
             <BreadcrumbItem className="bc-prev text-success pl-0"><Link to="/" className="site-link">Home</Link></BreadcrumbItem>
@@ -11,10 +11,20 @@ export const SiteCrumbs = ({ siteLocation }) => {
         </Breadcrumb>
     );
 }
+export const FaqCrumbs = () => {
+    return (
+        <Breadcrumb className="breadcrumb mb-0" style={{postion: "fixed"}}>
+            <BreadcrumbItem className="bc-prev text-success pl-0 "><a href="#general" className="faq-link">General</a></BreadcrumbItem>
+            <BreadcrumbItem active className="text-primary bc-now"><a href="#volunteer" className="faq-link">Volunteer</a></BreadcrumbItem>
+            <BreadcrumbItem active className="text-primary bc-now"><a href="#sponsor" className="faq-link">Sponsor</a></BreadcrumbItem>
+            <BreadcrumbItem active className="text-primary bc-now"><a href="#donate" className="faq-link">Donate</a></BreadcrumbItem>
+        </Breadcrumb>
+    );
+}
 
 export const PageTitle = ({ pageTitle }) => {
     return (
-        <Fade bottom cascade>
+        <Fade bothrefm cascade>
             <h1 className="text-center text-primary">{pageTitle}</h1>
         </Fade>
     );
@@ -23,7 +33,7 @@ export const PageTitle = ({ pageTitle }) => {
 const PageHeader = ({ siteLocation, pageTitle }) => {
     return (
         <div className="container">
-            <Row>
+            <Row style={{position: 'sticky'}}>
                 <Col>
                     <SiteCrumbs siteLocation={siteLocation} />
                     <PageTitle pageTitle={pageTitle} />
