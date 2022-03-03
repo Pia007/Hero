32cb61f7935b5ae1f41a07bd8c3d0a61e31a7f51
Pia <piatorain@yahoo.com>
2022-03-02 17:49:20 -0800

    removed FaqCrumbs

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Row, Col } from 'reactstrap';
-import { useLocation, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 
 export const SiteCrumbs = ({ siteLocation }) => {
@@ -9,33 +9,6 @@ export const SiteCrumbs = ({ siteLocation }) => {
             <BreadcrumbItem className="bc-prev text-success pl-0"><Link to="/" className="site-link">Home</Link></BreadcrumbItem>
             <BreadcrumbItem active className="text-primary bc-now">{siteLocation}</BreadcrumbItem>
         </Breadcrumb>
-    );
-}
-export const FaqCrumbs = () => {
-    const location = useLocation();
-    const { pathname } = location;
-    const faqLocation = pathname.split('/')[1];
-
-    return (
-        <div className='container p-0'>
-            <ul className="d-flex justify-content-start list-unstyled faqLinks" >
-                <li className={faqLocation[1] === "" ? "active" : "visited"}>
-                    <a href="#general" className="btn btn-link mx-0 faq-link pl-0">General</a>
-                </li>
-                <li className={faqLocation[1] === "volunteer" ? "active" : ""}>
-                    <a href="#volFaq" className="btn btn-link  mx-0 faq-link">Volunteer</a>
-                </li>
-                <li className={faqLocation[1] === "sponsor" ? "active" : ""}>
-                    <a href="#sponsor" className="btn btn-link  mx-0 faq-link">Sponsor</a>
-                </li>
-                <li className={faqLocation[1] === "donate" ? "active" : ""}>
-                    <a href="#donate" className="btn btn-link mx-0 faq-link">Donate</a>
-                </li>
-                <li className={faqLocation[1] === "top" ? "active" : ""}>
-                    <a href="#top" className="btn btn-link mx-0 faq-link">Back to Top</a>
-                </li>
-            </ul>
-        </div>
     );
 }
 
