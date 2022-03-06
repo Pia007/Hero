92a5cee47f01c35983fe34fafe419ba8a51b71ea
Pia <piatorain@yahoo.com>
2022-03-05 20:54:09 -0800

    SEO

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,10 +2,12 @@
 import PageHeader from '../components/PageHeader';
 import ContactInfo from '../components/ContactInfo';
 import ContactForm from '../components/ContactForm';
+import { SEO } from '../components/SEO';
 
 const Contact = () => {
         return (
             <div >
+                <SEO title="Contact | Host A Healthcare Hero" description={"description"}  content={"Contact form page of the website"}/>
                 <PageHeader siteLocation={"Contact"} pageTitle={"Contact Us"} />
                 <ContactInfo />
                 <ContactForm />
--- src/pages/Involved.jsx
+++ src/pages/Involved.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PageHeader from '../components/PageHeader';
 import InvolvedBody from '../components/InvolvedBody';
+import { SEO } from '../components/SEO';
 
 const Involved = (props) => {
     return (
         <div className="" style={{overflowX: 'hidden'}}>
+            <SEO title={"Get Involved | Host A Healthcare Hero"}  description={"description"} content={"Get Involved page of the website"} />
             <PageHeader siteLocation={"Get Involved"} pageTitle={"Get Involved"} />
             <InvolvedBody />
         </div>
