a4856947a2bf9540be89181cd6af3a600d6da4fa
Pia <piatorain@yahoo.com>
2022-03-06 22:04:46 -0800

    update SEO

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Main from './components/Main';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import './App.css';
 
 // In configuresStore.js: the function returns the redux store 
@@ -16,14 +16,16 @@ class App extends Component {
           // makes the redux store available to all connected components that are children of app
             <Provider store={store}>
                 <BrowserRouter >
-                    <div className="App">
-                        <Helmet>
-                            <title>Host A Heathcare Hero</title>
-                            <meta name="description" content="Volunteer to give back to healthcare heroes" />
-                            <meta name="theme-color" content="#008f68" />
-                        </Helmet>
-                        <Main  />
-                    </div>
+                    <HelmetProvider>
+                        <div className="App">
+                            <Helmet>
+                                <title>Host A Heathcare Hero</title>
+                                <meta name="description" content="Volunteer to give back to healthcare heroes" />
+                                <meta name="theme-color" content="#008f68" />
+                            </Helmet>
+                            <Main  />
+                        </div>
+                    </HelmetProvider>
                 </BrowserRouter>
             </Provider>
         );
