8f14012f80e6dd4b6f329629741fa9a063f76873
Pia <piatorain@yahoo.com>
2022-03-10 10:49:57 -0800

    Updated accordion and universal btn responsiveness

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export const Accordion = ({ name, description, titleHolder, titleContent}) => {
-    const [isActive, setIsActive] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         
         <div className="mission-text px-3 px-sm-0">
-            <div className={titleHolder} onClick={() => setIsActive(!isActive)}>
+            <div className={titleHolder} onClick={() => setIsOpen(!isOpen)}>
                 <h4 className="text-light mb-0"><strong className={titleContent}>{name}</strong></h4>
-                <div >{isActive ? '-' : '+'}</div>
+                <div >{isOpen ? '-' : '+'}</div>
             </div>
-                {isActive && 
+                {isOpen && 
                     <div className="text-primary p-1 description mb-1 px-3">
                         {name}, {description}
                     </div>
