1bfacee9efd0c4b9aaf53607a8d77cf1569d389a
Pia <piatorain@yahoo.com>
2022-03-13 22:50:01 -0700

    fixed layout

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -6,7 +6,7 @@ import Fade from 'react-reveal/Fade';
 export const SiteCrumbs = ({ siteLocation }) => {
     return (
         <Breadcrumb className="breadcrumb mb-0 sticky" id="Top">
-            <BreadcrumbItem className="bc-prev text-success pl-0"><Link to="/" className="site-link">Home</Link></BreadcrumbItem>
+            <BreadcrumbItem className="bc-prev text-success "><Link to="/" className="site-link">Home</Link></BreadcrumbItem>
             <BreadcrumbItem active className="text-primary bc-now">{siteLocation}</BreadcrumbItem>
         </Breadcrumb>
     );
@@ -14,9 +14,11 @@ export const SiteCrumbs = ({ siteLocation }) => {
 
 export const PageTitle = ({ pageTitle }) => {
     return (
-        <Fade bottom cascade>
-            <h1 className="text-center text-primary">{pageTitle}</h1>
-        </Fade>
+        <div className="container-fluid">
+            <Fade bottom cascade>
+                <h1 className="text-center text-sky">{pageTitle}</h1>
+            </Fade>
+        </div>
     );
 }
 
@@ -24,7 +26,7 @@ const PageHeader = ({ siteLocation, pageTitle }) => {
     return (
         <div className="container mb-3">
             <Row style={{position: 'sticky'}}>
-                <Col>
+                <Col className="px-0">
                     <SiteCrumbs siteLocation={siteLocation} />
                     <PageTitle pageTitle={pageTitle} />
                 </Col>
