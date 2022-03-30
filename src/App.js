f43f66a7265169bbfe6dd84a660ea48d782d1cb8
Pia <piatorain@yahoo.com>
2022-03-30 12:02:08 -0700

    Used BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Main from './components/Main';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 import { SEO } from './components/SEO';
@@ -11,14 +11,14 @@ const store = ConfigureStore();
 function App() {
     return (
         <Provider store={store}>
-            <HashRouter >
+            <BrowserRouter >
                 <HelmetProvider>
                     <div className='App'>
                         <SEO title={'Host A Healthcare Hero '} description={'description'} content={'Volunteer to give back to a healthcare hero.'} />
                         <Main style={{maxWidth: '100vw', paddingX: '10'}} />
                     </div>
                 </HelmetProvider>
-            </HashRouter>
+            </BrowserRouter>
         </Provider>
     );
 }
